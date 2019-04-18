aea5d76d1134d6d44aa390586c3049264570346b
ec3bd <ec3bd@virginia.edu>
2019-04-17 23:53:54 -0400

    slight updates

--- translate.py
+++ translate.py
@@ -1,5 +1,4 @@
-#Setting up a pipeline for the task:
-#english article -> machine translated -> stanford nlp -> multiple sentiment scores 
+#Setting up a pipeline for the task: #english article -> machine translated -> stanford nlp -> multiple sentiment scores
 
 import requests
 import json
@@ -8,7 +7,7 @@ import wikipedia
 from stanfordcorenlp import StanfordCoreNLP
 import subprocess
 
-
+languagelist = ['en', 'zh', 'es', 'de', 'ru', 'ja']
 
 
 def main():
@@ -17,14 +16,23 @@ def main():
 	if len(sys.argv) > 1:
 		for arg in sys.argv[1:]:
 			article = fetch_article(arg)
-			stanford_corenlp(article)
-			translated = translate(article)
+			print(article)
+			# stanford_corenlp(article)
+			# translated = translate(article)
 			# sentiments = stanford_corenlp(translated)
 			# print(arg)
 			# print(sentiments)
 
 	else:
 		print("Please enter at least one Wikipedia link to analyse.")
+
+def show_named_entities():
+	overall_entities = {}
+	with open('prospectiveTopics.txt', 'r') as topics:
+		for line in topics:
+			fetch_article(line)
+
+
 
 
 #given a link to a wikipedia article, 
@@ -47,44 +55,46 @@ def fetch_article(link):
 
 	print("Num characters in article: "+ str(len(content)))
 
-	return {title : content}
+	retdict = {'en': content}
 
-#input: dictionary with {title:english article text as string}
+	#get languages this article is in
+	param_dict = {"action":"query",
+					"prop":"langlinks",
+					"format":"json",
+					"redirects":1,
+					"titles":title}
+	req = requests.get("https://en.wikipedia.org/w/api.php", params=param_dict)
+	results = req.json()
+	# print(results)
+	# print(req.url)
+
+	for pageid, info in results['query']['pages'].items():
+		for lang in info['langlinks']:
+			print(lang)
+			if lang['lang'] in languagelist: #only get chinese, german, spanish
+				wikipedia.set_lang(lang['lang'])
+				page = wikipedia.page(lang['*'])
+				content = page.content
+				retdict[lang['lang']] = content
+
+	return retdict
+
+#input: dictionary with {'en:english article text as string, 'zh': chinese article text} etc etc
 #output: dictionary with keys being various two-letter language codes and items being the translated article text
 def translate(article):
 
-	for title in article:
-		#get languages this article is in
-		param_dict = {"action":"query",
-						"prop":"langlinks",
-						"format":"json",
-						"redirects":1,
-						"titles":title}
-		req = requests.get("https://en.wikipedia.org/w/api.php", params=param_dict)
-		results = req.json()
-		print(results)
-		print(req.url)
-
-		languages = {}
-		for pageid, info in results['query']['pages'].items():
-			for lang in info['langlinks']:
-				languages[lang['lang']] = {"title": lang['*']}
-
-		print(languages)
-
-
-		#get translated articles
-		for lang in languages:
-			wikipedia.set_lang(lang)
-			page = wikipedia.page(languages[lang]['title'])
-			content = page.content
-			#print(content)
-			data = {"q":content,
-					"target":"en",
-					"source":lang,
-					"format":"text",
-					"key":"NO KEY HERE UNTIL WE GET A STORAGE MECHANISM GOING, or compress foriegn language to "}
-			#req = requests.post("https://translation.googleapis.com", data=data)
+	#get translated articles
+	for lang in article:
+		wikipedia.set_lang(lang)
+		page = wikipedia.page(languages[lang]['title'])
+		content = page.content
+		#print(content)
+		data = {"q":content,
+				"target":"en",
+				"source":lang,
+				"format":"text",
+				"key":"NO KEY HERE UNTIL WE GET A STORAGE MECHANISM GOING, or compress foriegn language to "}
+		#req = requests.post("https://translation.googleapis.com", data=data)
 
 
 def stanford_corenlp(article):
